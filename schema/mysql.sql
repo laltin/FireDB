bb08279b57aadf0daca1403ad4866d2b6c9de993
Lütfi Altın <lutfi.altin@gmail.com>
2021-12-17 18:21:44 +0100

    improve mysql performance

--- schema/mysql.sql
+++ schema/mysql.sql
@@ -1,14 +1,14 @@
 CREATE TABLE `json` (
-  `path0` varchar(120) NOT NULL,
-  `path1` varchar(120) DEFAULT NULL,
-  `path2` varchar(120) DEFAULT NULL,
-  `path3` varchar(120) DEFAULT NULL,
-  `path4` varchar(120) DEFAULT NULL,
-  `path5` varchar(120) DEFAULT NULL,
-  `path6` varchar(120) DEFAULT NULL,
-  `path7` varchar(120) DEFAULT NULL,
-  `path8` varchar(120) DEFAULT NULL,
-  `path9` varchar(120) DEFAULT NULL,
+  `path0` varchar(32) NOT NULL,
+  `path1` varchar(32) DEFAULT NULL,
+  `path2` varchar(32) DEFAULT NULL,
+  `path3` varchar(32) DEFAULT NULL,
+  `path4` varchar(32) DEFAULT NULL,
+  `path5` varchar(32) DEFAULT NULL,
+  `path6` varchar(32) DEFAULT NULL,
+  `path7` varchar(32) DEFAULT NULL,
+  `path8` varchar(32) DEFAULT NULL,
+  `path9` varchar(32) DEFAULT NULL,
   `type` varchar(15) NOT NULL,
   `bool_value` tinyint(1) DEFAULT NULL,
   `int_value` int(11) DEFAULT NULL,
@@ -18,5 +18,5 @@ CREATE TABLE `json` (
 
 -- indexes
 ALTER TABLE `json`
-  ADD UNIQUE KEY `path` (`path0`,`path1`,`path2`,`path3`,`path4`,`path5`,`path6`,`path7`,`path8`,`path9`) USING HASH;
+  ADD UNIQUE KEY `path` (`path0`,`path1`,`path2`,`path3`,`path4`,`path5`,`path6`,`path7`,`path8`,`path9`) USING BTREE;
 COMMIT;
